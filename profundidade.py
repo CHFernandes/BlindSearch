29ed850b1562b28acbaea2a5876b2d4674cbbdbd
Carlos Fernandes <chfernandes01@hotmail.com>
2021-04-02 17:23:28 +0000

    Profundidade buscando

--- profundidade.py
+++ profundidade.py
@@ -3,6 +3,7 @@ exitValue = 0
 depths = {}
 father = {}
 levels = {}
+foundValue = False
 
 citiesArray = [
     "Rostock",
@@ -56,23 +57,30 @@ citiesRoutes = {
    "Bonn":["Trier","Mainz","Köln" ],
 }
 
-def busca_em_profundidade(graph, selectedCity):
+def busca_em_profundidade(graph, selectedCity, selectedEndingCity):
     father[selectedCity] = None
-    call_to_busca_em_profundidade(graph, selectedCity, 1)
+    call_to_busca_em_profundidade(graph, selectedCity, 1, selectedEndingCity)
 
-def call_to_busca_em_profundidade(graph, city, level):
-    global entryValue, exitValue
+def call_to_busca_em_profundidade(graph, city, level, selectedEndingCity):
+    global entryValue, exitValue, foundValue
+    if foundValue == True:
+        return
+    if city == selectedEndingCity:
+        foundValue = True
+        # return
     entryValue += 1
     depths[city] = [entryValue, None]
     levels[city] = level
 
     children = 0
 
-    for neighbor in graph.get(city):
-        if not depths.get(neighbor):
-            father[neighbor] = city
-            children += 1
-            call_to_busca_em_profundidade(graph, neighbor, level + 1)
+    if foundValue == False:
+        for neighbor in graph.get(city):
+            if not depths.get(neighbor):
+                father[neighbor] = city
+                children += 1
+                call_to_busca_em_profundidade(graph, neighbor, level + 1, selectedEndingCity)
+
         
     exitValue += 1
     depths[city][1] = exitValue
@@ -92,6 +100,24 @@ selectedCityId-=1
 
 selectedCity = citiesArray[selectedCityId]
 
-busca_em_profundidade(citiesRoutes, selectedCity)
+print("Selecione uma cidade de parada \n")
 
+i = 0
+
+while i < len(citiesArray):
+    nextItem = str(i+1)
+    print(nextItem + " - " + citiesArray[i])
+    i+=1
+
+print()
+selectedEndingCityId = int(input())
+selectedEndingCityId-=1
+
+selectedEndingCity = citiesArray[selectedEndingCityId]
+
+print()
+
+busca_em_profundidade(citiesRoutes, selectedCity, selectedEndingCity)
+
+print(levels)
 print(depths)
